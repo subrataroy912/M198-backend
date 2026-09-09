39a3e8e207fb8f7cc46d380189720798c74d4887
subrataroy912 <subrata.lastchance@gmail.com>
2026-09-09 14:09:35 +0530

    Update SecurityConfig.java

--- src/main/java/com/M198/Majorproject/config/SecurityConfig.java
+++ src/main/java/com/M198/Majorproject/config/SecurityConfig.java
@@ -65,6 +65,8 @@ public class SecurityConfig {
                         "/v1/auth/oauth/**",
                         "/oauth2/**",
                         "/login/**",
+                    "/api/inngest",
+        "/api/inngest/**",
                         "/error")
                 .permitAll()
                 .requestMatchers("/v1/auth/logout").authenticated()
