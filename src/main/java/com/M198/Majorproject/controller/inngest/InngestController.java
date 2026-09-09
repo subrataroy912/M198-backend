c01282bb5f9c625ae9a101cb7e47ef22582df527
subrataroy912 <subrata.lastchance@gmail.com>
2026-09-09 14:34:22 +0530

    Update InngestController.java

--- src/main/java/com/M198/Majorproject/controller/inngest/InngestController.java
+++ src/main/java/com/M198/Majorproject/controller/inngest/InngestController.java
@@ -1,25 +1,11 @@
 package com.M198.Majorproject.controller.inngest;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Exposes the application Inngest serve endpoint.
- *
- * <p>The Inngest Spring adapter registers the function handler from
- * {@code InngestConfig}; this controller ensures that the configured serve URL
- * is also a Spring MVC route.  Inngest uses GET while discovering functions and
- * POST/PUT while invoking them, so all three methods must reach this endpoint.
- */
+import com.inngest.springboot.InngestController;
+
 @RestController
 @RequestMapping("/api/inngest")
-public class InngestController {
-
-    @RequestMapping(method = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT })
-    public ResponseEntity<Void> serve() {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
+public class InngestServeController extends InngestController {
 }
